e5ee863b5f2398b02840e1f80950b02205de6aa9
pawel054 <pawel.marcisz1@gmail.com>
2024-09-25 09:20:21 +0200

    Add AlbumObject Class

---
+++ AplikacjaDesktop/AlbumObject.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplikacjaDesktop
+{
+    public class AlbumObject
+    {
+        public string Artist { get; set; }
+        public string Album { get; set; }
+        public int SongsNumber {  get; set; }
+        public int Year { get; set; }
+        public int DownloadNumber {  get; set; }
+
+        public AlbumObject() { }
+        public AlbumObject(string artist, string album, int songsNumber, int year, int downloadNumber)
+        {
+            Artist = artist;
+            Album = album;
+            SongsNumber = songsNumber;
+            Year = year;
+            DownloadNumber = downloadNumber;
+        }
+    }
+}
